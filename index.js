4887bfb0e2380bcc9f993788cb5b513e06ac5bc5
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-02 17:52:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return "Happy Independence Day, you!"
 }
 
 function holidayCountdown(holiday, days){
