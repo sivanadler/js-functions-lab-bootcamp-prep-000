149ac09f2b3938e8d657425a01c1c5105133ddcf
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-02 17:47:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// // write your code below!
-// function happyHolidays() {
-//   return "Happy holidays!"
-// }
+// write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
 
-// function happyHolidaysTo(name) {
-//   return "Happy holidays, you!"
-// }
+function happyHolidaysTo(name) {
+  return "Happy holidays, you!"
+}
 
 function happyHolidaysTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
 }
 
-// function holidayCountdown(holiday, days){
-//   return "It's ${days} days until ${holiday}!"
-// }
+function holidayCountdown(holiday, days){
+  return "It's ${days} days until ${holiday}!"
+}
