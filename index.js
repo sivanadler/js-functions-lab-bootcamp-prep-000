bcb92837c1695f982e478dc829c3287ee2b8cd2f
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-02 17:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@
 //   return "Happy holidays, you!"
 // }
 
-// function happyHolidaysTo(holiday, name) {
-//   return "Happy ${holiday}, you!"
-// }
+function happyHolidaysTo(holiday, name) {
+  return "Happy ${holiday}, you!"
+}
 
 // function holidayCountdown(holiday, days){
 //   return "It's ${days} days until ${holiday}!"
